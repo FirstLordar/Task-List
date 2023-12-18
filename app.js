5b4875ddbac4b7c3a04ecfa75fc9688e4a87fd44
FirstLordar <arzhan2517@gmail.com>
2023-12-18 23:34:16 +0300

    check toggle & index
    Can check data-atributes for event listner

---
+++ app.js
@@ -0,0 +1,63 @@
+const inputElement = document.getElementById('title')
+const createBtn = document.getElementById('create')
+const listElement = document.getElementById('list')
+
+const notes = [{
+    title:'Выучить блок промассивы',
+    comleted: false
+},
+{
+    title:'Выучить Теорию Обьектов',
+    comleted: true
+} 
+]
+
+
+createBtn.onclick = function () {
+    if (inputElement.value.length === 0) {
+        return
+    }
+    const newNote = {
+        title: inputElement.value, 
+        comleted: false
+    }
+    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(newNote))
+    notes.push(newNote)
+    render()
+inputElement.value = '';
+}
+
+listElement.onclick = function(event) {
+    if (event.target.dataset.index) {
+        const index = parseInt(event.target.dataset.index)
+        const type = event.target.dataset.type
+
+        if (type === 'toggle') {
+            notes[index].comleted = !notes[index].comleted
+        }
+        else if (type === 'remove') {
+            console.log('remove', index)
+        }
+    }
+}
+
+function getNoteTemplate(note, index) {
+   return `
+    <li
+        class="list-group-item d-flex justify-content-between align-items-center"
+    >
+        <span  class= "${note.comleted ? 'text-decoration-line-through' : ''}">${note.title}</span>
+        <span>
+        <span class="btn btn-small btn-${note.comleted ? 'warning' : 'success'}" data-index="${index}" data-type= "toggle">&check;</span>
+        <span class="btn btn-small btn-danger" data-index="${index}" data-type= "remove">&times;</span>
+        </span>
+    </li>`
+}
+function render () {
+    listElement.innerHTML = ''
+    for(i = 0; i < notes.length; i++) {
+        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i))
+    }
+}
+
+render()
